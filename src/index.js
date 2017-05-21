e9aa418d0b7e4e8ba9c950b01bae54ae5f79658b
Maxim Chirkin <fs0ul92@gmail.com>
2017-05-21 15:02:31 +0300

    remove test console.log

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import formatNumber from './format_number';
 
-console.log(formatNumber(1234567.89));
-console.log(formatNumber(100,23));
-console.log(formatNumber('12345,67'));
-
-console.log(formatNumber('asd'));
-
-console.log(formatNumber(-100), formatNumber(-1000), formatNumber('- 100'), formatNumber('-1000,23'));
+export default formatNumber;
