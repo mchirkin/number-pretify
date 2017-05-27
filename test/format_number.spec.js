c4cc61e25831caeda8ae0b66cb1bd0d4c10a3077
Maxim Chirkin <fs0ul92@gmail.com>
2017-05-27 15:13:42 +0300

    test options seperatly

--- test/format_number.spec.js
+++ test/format_number.spec.js
@@ -9,13 +9,49 @@ describe('formatNumber', function() {
     expect(formatNumber('-100')).to.equal('- 100');
     expect(formatNumber('100,23')).to.equal('100,23');
     expect(formatNumber(1000.23)).to.equal('1 000,23');
-    expect(formatNumber('10000,1267', {
-      precision: 2,
-      decimalMark: '.',
-    })).to.equal('10 000.13');
+    expect(formatNumber(1000000)).to.equal('1 000 000');
+  });
+
+  it('delimeter option should work correctly', function() {
     expect(formatNumber(1000000.17, {
       delimeter: '.',
     })).to.equal('1.000.000,17');
+
+    expect(formatNumber(1000000.17, {
+      delimeter: '##',
+    })).to.equal('1##000##000,17');
+  });
+
+  it('precision option should work correctly', function() {
+    expect(formatNumber('10000,1267', {
+      precision: 2,
+    })).to.equal('10 000,13');
+  });
+
+  it('decimalMark option should work correctly', function() {
+    expect(formatNumber(10000.23, {
+      decimalMark: '-',
+    })).to.equal('10 000-23');
+  });
+
+  it('prefix option should work correctly', function() {
+    expect(formatNumber(1000, {
+      prefix: '$',
+    })).to.equal('$1 000');
+    expect(formatNumber(-1000, {
+      prefix: '$ ',
+    })).to.equal('$ - 1 000');
+    expect(formatNumber(1000, {
+      prefix: '$',
+      prefixAfterNegativeSign: true,
+    })).to.equal('$1 000');
+    expect(formatNumber(-1000, {
+      prefix: '$',
+      prefixAfterNegativeSign: true,
+    })).to.equal('- $1 000');
+  });
+
+  it('suffix option should work correctly', function() {
     expect(formatNumber(1000, {
       suffix: '$',
     })).to.equal('1 000$');
